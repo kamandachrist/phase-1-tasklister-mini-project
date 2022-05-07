9c5b5e643bc01123c0d88940b081e65ed1810611
Christ Kamanda <kamandachristo@gmail.com>
2022-05-07 15:17:24 -0400

    phase-1-tasklister-mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
+// document.querySelector("#submit").addEventListener("input", function(event) {
+//   document.getElementById("list").innerHTML += "Create New Task <br>";
+//   event.preventDefault();
+// }, false);
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector(`form`).addEventListener(`submit`, (e) => {
+    e.preventDefault()
+    console.log(e.target.text)
+  })  
+
+//   // your code here
 });
+
+
+
+//function suppressEvent(description) {
+  //console.log(description)
+
+//}
